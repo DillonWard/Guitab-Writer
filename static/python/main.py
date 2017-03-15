8b28ff0f4e3b2013c395d439e889d70ea895266e
Dillon Ward <Dillonward13@gmail.com>
2017-03-15 15:49:10 +0000

    Project reset to MEAN stack

--- static/python/main.py
+++
@@ -1,9 +0,0 @@
-import flask as myflask
-from flask import Flask, request, render_template, url_for, json
-import couchdb
-
-app = Flask(__name__)
-
-@app.route('/')
-def index():
-    return render_template("Index.html")
