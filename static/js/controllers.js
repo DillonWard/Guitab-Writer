7f4f1801d19943dfea1e66219c0488e6da0bc38b
Dillon Ward <Dillonward13@gmail.com>
2017-03-16 15:39:00 +0000

    images/buttons are displaying

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -1,8 +1,19 @@
 angular.module('app.controllers',[])
 
 .controller('mainCtrl', function($scope, $sce) {
-  $scope.guitarImg = $sce.trustAsResourceUrl('static/img/guitar.png');
+ 
+// $scope.guitarImg = $sce.trustAsResourceUrl('static/img/fret.png');
 
+// myFrets =  $sce.trustAsResourceUrl('static/img/fret.png');
+
+$scope.fret = {source: 'static/img/fret.png'};
+
+$scope.row = 12;
+$scope.column = 6;
+
+$scope.getNumber = function(number){
+  return new Array(number)
+}
 
  var tab = {
     E: [],
@@ -15,8 +26,14 @@ angular.module('app.controllers',[])
 
   $scope.tab = tab;
 
-  $scope.writeTab = function(){
-    tab ['E'] += "--1";
+  $scope.wait = function(){
+    tab ['E'] += "--";
+    tab ['A'] += "--";
+    tab ['D'] += "--";
+    tab ['G'] += "--";
+    tab ['B'] += "--";
+    tab ['e'] += "--";
     $scope.tab = tab;
   }
+
 });
