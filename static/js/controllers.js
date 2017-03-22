e75e65bb542bc9ef287231ca90c0776c04d8e13d
Dillon Ward <Dillonward13@gmail.com>
2017-03-22 14:19:07 +0000

    Buttons append text, text is formatted properly

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -12,19 +12,128 @@ angular.module('app.controllers',[])
   };
 
 $scope.fret = {source: 'static/img/fret.png'};
-$scope.row = 12;
-$scope.column = 6;
-
 
 $scope.writeTab = function(row, column){
-  $scope.test = row + " "  + column;
-  console.log($scope.test)
+
+  if(row === 0){
+    tab ['E'] += "--" + column + "--";
+    tab ['A'] += "-----";
+    tab ['D'] += "-----";
+    tab ['G'] += "-----";
+    tab ['B'] += "-----";
+    tab ['e'] += "-----";
+  }
+
+  else if(row === 1 && column < 9){
+    tab ['E'] += "-----";
+    tab ['A'] += "--" + column + "--";
+    tab ['D'] += "-----";
+    tab ['G'] += "-----";
+    tab ['B'] += "-----";
+    tab ['e'] += "-----";
+  }
+
+  else if (row === 2 && column < 9){
+    tab ['E'] += "-----";
+    tab ['A'] += "-----";
+    tab ['D'] += "--" + column + "--";
+    tab ['G'] += "-----";
+    tab ['B'] += "-----";
+    tab ['e'] += "-----";
+  }
+
+  else if(row === 3 && column < 9){
+    tab ['E'] += "-----";
+    tab ['A'] += "-----";
+    tab ['D'] += "-----";
+    tab ['G'] +="--" + column + "--";
+    tab ['B'] += "-----";
+    tab ['e'] += "-----";
+  }
+
+  else if(row === 4 && column < 9){
+    tab ['E'] += "-----";
+    tab ['A'] += "-----";
+    tab ['D'] += "-----";
+    tab ['G'] += "-----";
+    tab ['B'] += "--" + column + "--";
+    tab ['e'] += "-----";
+  }
+
+  else if(row === 5 && column < 9){
+    tab ['E'] += "-----";
+    tab ['A'] += "-----";
+    tab ['D'] += "-----";
+    tab ['G'] += "-----";
+    tab ['B'] += "-----";
+    tab ['e'] += "--" + column + "--";
+  }
+  
+  // FRETS ABOVE 9 NEED AN EXTRA LINE
+
+  else if(row === 0 && column > 9){
+    tab ['E'] += "--" + column + "--";
+    tab ['A'] += "------";
+    tab ['D'] += "------";
+    tab ['G'] += "------";
+    tab ['B'] += "------";
+    tab ['e'] += "------";
+  }
+
+ else if(row === 1 && column > 9){
+    tab ['E'] += "------";
+    tab ['A'] += "--" + column + "--";
+    tab ['D'] += "------";
+    tab ['G'] += "------";
+    tab ['B'] += "------";
+    tab ['e'] += "------";
+  }
+
+  else if(row === 2 && column > 9){
+    tab ['E'] += "------";
+    tab ['A'] += "------";
+    tab ['D'] += "--" + column + "--";
+    tab ['G'] += "------";
+    tab ['B'] += "------";
+    tab ['e'] += "------";
+  }
+
+ else if(row === 3 && column > 9){
+    tab ['E'] += "------";
+    tab ['A'] += "------";
+    tab ['D'] += "------";
+    tab ['G'] += "--" + column + "--";
+    tab ['B'] += "------";
+    tab ['e'] += "------";
+  }
+ else if(row === 4 && column > 9){
+    tab ['E'] += "------";
+    tab ['A'] += "------";
+    tab ['D'] += "------";
+    tab ['G'] += "------";
+    tab ['B'] += "--" + column + "--";
+    tab ['e'] += "------";
+  }
+ else if(row === 5 && column > 9){
+    tab ['E'] += "------";
+    tab ['A'] += "------";
+    tab ['D'] += "------";
+    tab ['G'] += "------";
+    tab ['B'] += "------";
+    tab ['e'] += "--" + column + "--";
+  }
+
+  
+  $scope.noteLog = row + "/"  + column;
+  console.log($scope.noteLog)
+  $scope.tab = tab;
 }
+
 
 $scope.getNumber = function(number){
   return new Array(number)
+  console.log($scope.number)
 }
-  $scope.tab = tab;
 
   $scope.wait = function(){
     tab ['E'] += "--";
@@ -35,5 +144,4 @@ $scope.getNumber = function(number){
     tab ['e'] += "--";
     $scope.tab = tab;
   }
-
 });
